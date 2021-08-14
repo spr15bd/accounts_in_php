b828746cdf4f6578b5037277b406134003d3b339
Bevan Dady <bevandady@hotmail.com>
2021-08-14 23:45:55 +0100

    established database connection in index.php

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<?php
+	// connect to database
+	$conn = mysqli_connect('localhost', '', '', 'accounts');
+	
+	// check connection
+	if (!$conn) {
+		echo "Conection error: " . mysqli_connect_error();
+	}
+
+?>
+
+
 <!doctype html>
 
 
