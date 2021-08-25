dcbe7ae8aee794c4b3f3ec08c60e78fe571ff16b
Bevan Dady <bevandady@hotmail.com>
2021-08-25 23:38:08 +0100

    Changed from materilize to bootstrap 5, amendments to supplier review layout

--- templates/header.php
+++ templates/header.php
@@ -1,6 +1,7 @@
 <head>
 	<title>Accounts System</title>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+	<!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">-->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 	<style type="text/css">
 		.brand{
 			background: #adbbec;
@@ -16,12 +17,23 @@
 	</style>
 </head>
 	<body class="grey lighten-2">
-		<nav class="white" z-depth-3>
-			<div class="container">
-				<a href="#" class="brand-logo brand-text">An accounting system</a>
-				<ul id="nav-mobile" class="right hide-on-small-and-down">
-					<li><a href="index.php?view=supplier_review" class="btn brand z-depth-0">Supplier Review</a></li>
-					<li><a href="add_invoices.php" class="btn brand z-depth-0">Add invoice</a></li>
-				</ul>
+		<div id="app">
+			<div class="container-fluid">
+				<div class="row bg-dark">
+					<div class="col-lg-12>
+						<nav class="white" z-depth-3>
+							<div class="container">
+								<p class="text-center text-light display-5 p-8">
+									An accounting system
+								</p>
+								<ul id="nav-mobile" class="right hide-on-small-and-down">
+									<li><a href="index.php?view=supplier_review" class="btn brand z-depth-0">Supplier Review</a></li>
+									<li><a href="add_invoices.php" class="btn brand z-depth-0">Add invoice</a></li>
+								</ul>
+							</div>
+
+						</nav>
+					</div>
+				</div>
 			</div>
-		</nav>
+		</div>
