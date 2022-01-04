edd1aa8ded9b45e48206b4ae7708000f9ce3c4c5
bevandady <bevandady@hotmail.com>
2022-01-04 22:10:43 +0000

    Updated app.js

--- app.js
+++ app.js
@@ -23,16 +23,13 @@ const app = Vue.createApp({
                             let payload = { data: { supplierName: supplier } }
                             const res = await axios.post("database_queries.php", payload )
                             const {results} = res
-                            console.log("app.js->getSupplier():  got database_queries.php", res);   
-                            console.log("vue:  Success getting database_queries response")
+                        
                             this.invoices =  res.data
                         },
                         async update() {
-                            console.log("updating ", this.supplier, this.invoiceNumber, this.invoiceDate, this.invoiceDescription, this.office, this.overhead, this.invoiceAmount)
                             let payload = {info: { supplier: this.supplier, invoiceNumber: this.invoiceNumber, invoiceDate: this.invoiceDate, invoiceDescription: this.invoiceDescription, office: this.office, overhead: this.overhead, invoiceAmount: this.invoiceAmount }}
                             const res = await axios.post("database_queries.php", payload )
                             const {results} = res
-                            console.log("app.js->update():  got database_queries.php: ", res);   
                         }
                     }
 })
