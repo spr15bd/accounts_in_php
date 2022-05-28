1f8cf349cf910677332d6a38b529a937877005b8
bevandady <bevandady@hotmail.com>
2022-05-28 23:58:59 +0100

    Modified app.js, index.html

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = Vue.createApp({
                             },
                             checkedInvoices: [],
                             vendorInvoices: null,
-                            invoices: [ { id: Number, supplier: null, number: null, date: null, description: null, office: null, overhead: null, amount: parseFloat(0.00), paid: parseFloat(0.00) } ],
+                            invoices: [ { id: Number, supplier: null, number: null, date: null, description: null, office: null, overhead: null, amount: parseFloat(0.00).toFixed(2), paid: parseFloat(0.00).toFixed(2) } ],
                             supplier: null,
                             paymentsSummaryPage: false,
                             paymentsSupplier: null,
