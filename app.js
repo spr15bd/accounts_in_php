8f37ceee2b23090bde46e6e31d10c2466ed8e972
bevandady <bevandady@hotmail.com>
2022-07-25 23:48:25 +0100

    Modified app.js, index.html

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ const app = Vue.createApp({
                             invoiceAmount: null,
                             statusMessage: null,
                             paymentProcessing: false,
+                            selected: false,
                             suppl: null
                         }
                     },
