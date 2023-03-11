9c97f007abca52734942bfc65d2f83e1d73e0ff9
bevandady <bevandady@hotmail.com>
2023-03-11 23:58:08 +0000

    Modified app.js

--- app.js
+++ app.js
@@ -195,10 +195,10 @@ const app = Vue.createApp({
                 let foundInv = this.allInvoices.find(inv=>inv.id===sp)
                 console.log("inv it belongs to ", foundInv)
                 // sum over supplier
-                if (!this.suppliers[foundInv?.supplier] || this.suppliers[foundInv?.supplier]===undefined) {
+                if (!this.suppliers[foundInv?.supplier]) {
                     this.suppliers[foundInv?.supplier] = Number(0)
+                                    console.log("created new supplier, ", foundInv?.supplier)
                 } else {
-                    console.log("new supplier to add")
                     console.log("about to add ", foundInv?.amount, "with type ", typeof foundInv?.amount, " adding to ", foundInv?.supplier)
                     this.suppliers[foundInv?.supplier] += Number(foundInv?.amount)                          
                 }
