9f5fad03d83d297d38be0786db573301f1412063
Bevan Dady <bevandady@hotmail.com>
2023-04-10 22:34:32 +0100

    Modified app.js

--- app.js
+++ app.js
@@ -110,8 +110,10 @@ const app = Vue.createApp({
             // if index is selected, return amount outstanding on invoice
             // otherwise return zero
             let selectedInvoice
+	    console.log("vendor invoices: ", this.vendorInvoices)
             if (this.selectedPayments.includes(idx)) {
-                let vendorInvoices = Object.values(this.vendorInvoices['Ulreco'])
+                let vendorInvoices = Object.values(this.vendorInvoices)['Ulreco']
+		
                 selectedInvoice = vendorInvoices.find(inv=>
                     inv.id === idx
                 )
