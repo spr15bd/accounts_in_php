b871b2a3de90cdaf77a596f1b4595f49f6375289
Bevan Dady <bevandady@hotmail.com>
2023-05-23 23:11:27 +0100

    Modified app.js, index.html

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ const app = Vue.createApp({
                         sum += Number(invoiceToAdd.amount)           
 					}
                 })
-                this.paymentsRecord[supplier] = sum
+                this.paymentsRecord.push(supplier=>sum)
                 console.log("paymentsRecord: ", this.paymentsRecord)
                 return sum
             }
