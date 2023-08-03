f7b3633d4cbcc5cec3106f668f83db88823a3a96
Bevan Dady <bevandady@hotmail.com>
2023-08-03 23:26:22 +0100

    Modified database_queries.php

--- database_queries.php
+++ database_queries.php
@@ -25,7 +25,7 @@
                 } else {
                     $str = implode(" ", $items['data']);
                     $query = "SELECT * FROM invoices i ";
-                    $query .="INNER JOIN paid p ON p.paid = i.id ";
+                    $query .="LEFT JOIN paid p ON p.paid = i.id ";
                     $query .="WHERE supplier='".$str."'";
                 }
                 echo $query;
