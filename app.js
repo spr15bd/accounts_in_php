b5606286e4149c19d32098937599b40130308e5a
Bevan Dady <bevandady@hotmail.com>
2023-08-10 23:31:01 +0100

    Modified app.js

--- app.js
+++ app.js
@@ -114,6 +114,7 @@ const app = Vue.createApp({
         reviewOutstandingSum(invoices) {
             let sum = 0
             invoices.forEach((invoice)=> {
+                console.log("invoice: ", invoice)
                 // temp - todo add in total outstanding on the inv, new table id and paid
                 sum += parseFloat(invoice.amount)
             })
