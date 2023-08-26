2980f83f29899ddbd3b00f47429585f7127c94c0
Bevan Dady <bevandady@hotmail.com>
2023-08-26 23:58:23 +0100

    Modified app.js

--- app.js
+++ app.js
@@ -44,6 +44,7 @@ const app = Vue.createApp({
             const res = await axios.post("database_queries.php", payload )
             if (res.data) {
                 this.vendorInvoices = res.data
+                console.log("Supplier query returned ", this.vendorInvoices)
                 this.vendorInvoices.forEach(invoice=>{
                     
                     invoice.amount = (Math.round(invoice.amount * 100) / 100).toFixed(2)
