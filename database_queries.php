a9880a59c58e82dc91b97fc1ca03ecf319816544
Bevan Dady <bevandady@hotmail.com>
2024-05-26 23:17:20 +0100

    Modified database_queries.php, index.html

--- database_queries.php
+++ database_queries.php
@@ -31,7 +31,7 @@
                     $query = str_replace("'s", "s", $query);
                 }
                 $sql_query = $conn->query($query);
-		var_dump("SQL Query: " . $sql_query);	
+		//var_dump("SQL Query: " . $sql_query);	
                 $invoices = array();
                 while ($row=$sql_query->fetch_assoc()){
                     array_push($invoices, $row);
